4e20d986bcc02bc4386f921f68e1c3664897af22
Yaroslav Samoilenko <yasik2255@live.com>
2024-07-17 10:01:19 +0300

    feat/HYB_removed_unused

--- src/createServer.js
+++ src/createServer.js
@@ -5,19 +5,14 @@ const express = require('express');
 const { usersRouter } = require('./routes/usersRouter');
 const userService = require('./services/usersService');
 
-const expenseService = require('./services/expensesService');
-const { expensesRouter } = require('./routes/expensesRouter');
-
 function createServer() {
   userService.init();
-  expenseService.init();
 
   const app = express();
 
   app.use(express.json());
 
   app.use('/users', usersRouter);
-  app.use('/expenses', expensesRouter);
 
   return app;
 }
